19ea73b4761aab35d79102d0693392cf6c637087
Yuta Fujii <yutayutakid21@gmail.com>
2020-07-21 15:52:44 +0900

    課題

---
+++ MVCMerge/Controller/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MVCMerge
+//
+//  Created by Yuta Fujii on 2020/07/21.
+//  Copyright © 2020 Yuta Fujii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    /*
+     Modelに定義されている構造体を使って、Personから別々の年齢と攻撃力を持ったピカチューを3体作成し、
+     UILabelをつないで表示してください。また、masterではなくOtherブランチからフォークし、Otherブランチへプルリクエストを送ってください。
+     */
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MVCMerge/Model/Person.swift
@@ -0,0 +1,14 @@
+//
+//  Model.swift
+//  MVCMerge
+//
+//  Created by Yuta Fujii on 2020/07/21.
+//  Copyright © 2020 Yuta Fujii. All rights reserved.
+//
+
+import Foundation
+
+struct Person {
+    
+  
+}
